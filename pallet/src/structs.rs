ef4a380c073c778528cb5e76b555a7f076759421
jaypan <pan@peaq.io>
2022-10-27 13:35:37 +0200

    Remove unused derive

--- pallet/src/structs.rs
+++ pallet/src/structs.rs
@@ -3,26 +3,10 @@ use scale_info::TypeInfo;
 use sp_core::RuntimeDebug;
 use sp_std::vec::Vec;
 
-#[cfg(feature = "std")]
-use serde::{Deserialize, Serialize};
-
-// #[cfg(feature = "std")]
-// use sp_rpc::number::NumberOrHex;
-
-
 /// Attributes of a DID.
 #[derive(
 	PartialEq, Eq, PartialOrd, Ord, Clone, Encode, Decode, Default, RuntimeDebug, TypeInfo,
 )]
-#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-// #[cfg_attr(
-// 	feature = "std",
-// 	serde(
-// 		rename_all = "camelCase",
-// 		bound(serialize = "BlockNumber: Serialize, Moment: Serialize"),
-// 		bound(deserialize = "BlockNumber: Deserialize<'de>, Moment: Deserialize<'de>")
-// 	)
-// )]
 pub struct Attribute<BlockNumber, Moment> {
 	pub name: Vec<u8>,
 	pub value: Vec<u8>,
